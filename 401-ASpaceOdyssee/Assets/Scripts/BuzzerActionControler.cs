c20abfdae93e7c92d25469a1f6d50a984f0e70e4
Arthus T <106975406+SuhtrA@users.noreply.github.com>
2022-09-09 11:21:42 +0200

    Upload_Main

---
+++ 401-ASpaceOdyssee/Assets/Scripts/BuzzerActionControler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BuzzerActionControler : MonoBehaviour
+{
+    public Material mActiveMaterial;
+    public Material mInactiveMaterial;
+    bool mIsActive = false;
+
+    public void BuzzerPressed(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            mIsActive = !mIsActive;
+            if (mIsActive)
+            {
+                GetComponent<MeshRenderer>().material = mActiveMaterial;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material = mInactiveMaterial;
+            }
+        }
+    }
+}
---
+++ 401-ASpaceOdyssee/Assets/Scripts/MIDIRead.cs
@@ -0,0 +1,28 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MIDIRead : MonoBehaviour
+{
+    public TextAsset mMidiFile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string content = mMidiFile.text;
+        string lines = content.Split("\n");
+        foreach (string line in lines) ;
+        
+        {
+            string ;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
